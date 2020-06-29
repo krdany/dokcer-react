479444985c1124c70e5ada730ad5b81587d8bdca
Dev Server <dev_server@webmark.co.il>
2020-06-30 00:56:42 +0300

    feature chagne

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Heloo!!123222
+            Heloo!!123222 change chgane feature
         </p>
         <a
           className="App-link"
